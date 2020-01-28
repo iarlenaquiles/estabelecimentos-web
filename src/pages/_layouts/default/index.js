e545cbc752ce717dea2cfd22601f48c176ac6204
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-28 11:38:17 -0300

    criando layout default

---
+++ src/pages/_layouts/default/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+import Header from '~/components/Header';
+import * as S from './styles';
+
+export default function DefaultLayout({ children }) {
+  return (
+    <S.Wrapper>
+      <Header />
+      {children}
+    </S.Wrapper>
+  )
+}
+
+DefaultLayout.propType = {
+  children: PropType.element.isRequired
+};
