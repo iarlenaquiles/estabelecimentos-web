96f05a1225f1de98f2ecd39284d1dc83db8db520
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-28 19:19:10 -0300

    ajustando caminho do index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import Signin from '~/pages/Signin';
-import Signup from '~/pages/Signup';
-import Dashboard from '~/pages/Dashboard';
+import Signin from '../pages/Signin';
+import Signup from '../pages/Signup';
+import Dashboard from '../pages/Dashboard';
 
 export default function Routes() {
   return (
